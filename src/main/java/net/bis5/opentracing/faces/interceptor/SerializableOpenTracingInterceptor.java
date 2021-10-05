42806f38dc6e67ed61198f675175ce85b5ad28e5
Takayuki Maruyama <bis5.wsys@gmail.com>
2021-10-05 23:21:37 +0000

    fix: Bump opentracing-api from 0.31.0 to 0.33.0
    fix #1

--- src/main/java/net/bis5/opentracing/faces/interceptor/SerializableOpenTracingInterceptor.java
+++ src/main/java/net/bis5/opentracing/faces/interceptor/SerializableOpenTracingInterceptor.java
@@ -73,27 +73,27 @@ public class SerializableOpenTracingInterceptor implements Serializable {
             }
         }
 
-        Scope scope = spanBuilder.startActive(true);
-        try {
+        Span span = spanBuilder.start();
+        try (Scope scope = tracer.scopeManager().activate(span)) {
             log.fine("Adding span context into the invocation context.");
-            ctx.getContextData().put(SPAN_CONTEXT, scope.span().context());
+            ctx.getContextData().put(SPAN_CONTEXT, span.context());
 
             if (contextParameterIndex >= 0) {
                 log.fine("Overriding the original span context with our new context.");
                 for (int i = 0 ; i < ctx.getParameters().length ; i++) {
                     if (ctx.getParameters()[contextParameterIndex] instanceof Span) {
-                        ctx.getParameters()[contextParameterIndex] = scope.span();
+                        ctx.getParameters()[contextParameterIndex] = span;
                     }
 
                     if (ctx.getParameters()[contextParameterIndex] instanceof SpanContext) {
-                        ctx.getParameters()[contextParameterIndex] = scope.span().context();
+                        ctx.getParameters()[contextParameterIndex] = span.context();
                     }
                 }
             }
 
             return ctx.proceed();
         } finally {
-            scope.close();
+            span.finish();
         }
     }
 
